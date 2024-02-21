bdd29cab87fe2bf31aa0ba6697b3a25e3b024089
Brice Colbert <brice.tech@gmail.com>
2024-02-20 16:49:10 -0800

    JavaScript Node Hello World

---
+++ JavaScript/Examples/node_hello_world/hello_world.js
@@ -0,0 +1,8 @@
+// A simple hello world program written in JavaScript/Node
+
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.end('Hello World!');
+}).listen(8080);
---
+++ JavaScript/Examples/node_hello_world/launch_and_visit_hello_world.sh
@@ -0,0 +1,3 @@
+echo "open the following URL in your browser http://localhost:8080"
+
+node hello_world.js
