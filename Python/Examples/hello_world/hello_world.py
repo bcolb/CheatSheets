900e5b4cee31d1f04851ebc4cdddfadf1e17b5fe
Brice Colbert <brice.tech@gmail.com>
2024-02-20 16:48:29 -0800

    Python hello world

---
+++ Python/Examples/hello_world/hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def hello_world():
+    '''A simple function that prints "Hello World!" to standard out.'''
+    print("Hello World!")
+
+if __name__ == '__main__':
+    ''' Executes when the script is run, i.e. "python3 hello_world.py".'''
+    hello_world()
